6281a663fdf5363d55f371ba51e5c817efee333a
yuriy <php_developer@zeo.ua>
2021-07-01 15:42:14 +0300

    add php insights

--- app/Http/Controllers/Api/v1/QuizController.php
+++ app/Http/Controllers/Api/v1/QuizController.php
@@ -1,67 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Models\Quiz;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
-class QuizController extends Controller
+final class QuizController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return Collection|Quiz[]
+     * @return array
      */
-    public function index()
+    public function index(): array
     {
         return Quiz::all();
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
